705d837f481161ea42eed4292744e5eae7ee30c9
slemchik03 <vadimmaster228@gmail.com>
2024-12-03 02:44:21 +0200

    fix: eslint fixes

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,16 +1,15 @@
-"use client"
+"use client";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { getQueryClient } from "./getQueryClient";
 import { PropsWithChildren } from "react";
 
-interface ProvidersProps extends PropsWithChildren {
-}
+type ProvidersProps = PropsWithChildren;
 
-export default function Providers({children}: ProvidersProps) {
-    return (
-        <QueryClientProvider client={getQueryClient()}>
-            {children}
-        </QueryClientProvider>
-    )
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <QueryClientProvider client={getQueryClient()}>
+      {children}
+    </QueryClientProvider>
+  );
 }
